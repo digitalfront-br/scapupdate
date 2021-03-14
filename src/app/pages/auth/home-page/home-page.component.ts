731ff7a9b1eb8b2fe9ee608dd21fe7d71581bf84
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-03-13 22:49:01 -0300

    [homepage]

--- src/app/pages/auth/home-page/home-page.component.ts
+++ src/app/pages/auth/home-page/home-page.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 import UIkit  from 'uikit';
 
+let pristine = true;
+
 @Component({
   selector: 'df-home-page',
   templateUrl: './home-page.component.html',
   styles: [
   ]
 })
+
 export class HomePageComponent implements OnInit {
 
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo) { 
+  }
 
   public loading = true;
+  public data;
   public personValues;
 
   ngOnInit(): void {
@@ -20,28 +25,62 @@ export class HomePageComponent implements OnInit {
     this.openModal();
   }
 
+ 
+
   closeModal(){
+    pristine = false;
     UIkit.modal('#steps').hide();
   }
 
   openModal(){
-    UIkit.modal('#steps').show();
+    if(pristine){
+      UIkit.modal('#steps').show();
+    }
   }
 
   loadPersonValue() {
     this.apollo.query({
       query: gql`
       { 
-        personValues(first: 250){
+        questions(first: 10){
+          count:paginatorInfo{
+            total
+          }
+          data{
+            title
+          }
+        }
+        movies:moviesBooks(first: 5, type: 1){
+          count:paginatorInfo{
+            total
+          }
+          data{
+            title
+          }
+        }
+        books:moviesBooks(first: 5, type: 2){
+          count:paginatorInfo{
+            total
+          }
+          data{
+            title
+          }
+        }
+        categories(first: 5){
+          count:paginatorInfo{
+            total
+          }
           data{
             id
             title
+            image
           }
         }
       }
       `
     }).subscribe((res) => {
-      this.personValues = res.data
+      console.log(res.data)
+      this.data = res.data
       this.loading = res.loading
     },(error) => {
       console.log(error);
